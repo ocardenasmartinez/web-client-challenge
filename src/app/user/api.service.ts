95dfa717ff6770f05be6726383e2e8003971c334
Octavio Cárdenas <ocardenas@subtel.gob.cl>
2019-09-04 12:20:36 -0400

    add

--- src/app/user/api.service.ts
+++ src/app/user/api.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+const httpOptions = {
+  headers: new HttpHeaders({'Content-Type': 'application/json'})
+};
+
+const url = 'http://localhost:9001/saveuser';
 
 @Injectable({providedIn: 'root'})
 export class ApiService {
-  constructor(private httpClient: HttpClient) {
+  constructor(private httpClient: HttpClient) {}
 
+  postUser(data) {
+    return this.httpClient.post(
+        url,
+        JSON.stringify(data),
+        httpOptions)
+      .subscribe(response => {
+        console.log(response);
+      });
   }
+
 }
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,17 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { ApiService } from './api.service';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
-export class UserComponent implements OnInit {
+export class UserComponent {
 
-  ngOnInit() {
-  }
+  constructor(private apiService: ApiService) {}
 
   onClickSubmit(data) {
-      console.log(data);
+    console.log(this.apiService.postUser({
+      name: "Octavio",
+      lastname: "Cárdenas",
+      birthdate: "09/01/2019",
+      contactNumber: "123456",
+      address: "address 1",
+      email: "ocardenas@subtel.cl",
+      country: "Chile"
+    }));
   }
 }
